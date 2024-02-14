efc10188ea2d2c77fa7e519daff8f28f34be4e79
Dhanush Chowdary <67065764+Dhanush2201@users.noreply.github.com>
2024-02-13 22:19:12 -0600

    Add files via upload

---
+++ Problem_0.py
@@ -0,0 +1,14 @@
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n-1) + fib(n-2)
+
+def debug_fib(n):
+    print(f"Calling fib({n})")
+    result = fib(n)
+    print(f"fib({n}) = {result}")
+    return result
+
+debug_fib(5)
